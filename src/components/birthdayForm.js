6ee0cec31006c87371d3fb0d3b382916f15a4f10
Heith <Heith.Robbins@gmail.com>
2020-07-22 14:08:19 -0600

    set up birthday from component set up date pick with moment.js

---
+++ src/components/birthdayForm.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import DatePicker from 'react-datepicker'
+import moment from 'moment';
+
+class Birthday extends Component {
+
+    constructor(props) {
+        super(props)
+
+        this.handleChange = this.handleChange.bind(this);
+
+        this.state = {
+            startDate: moment()
+        }
+
+    }
+
+    handleChange(date) {
+
+        this.setState({
+            startDate: date
+        })
+    }
+    
+
+    render() {
+        return (
+          <div>
+            <h1>Birthday!</h1>
+            <DatePicker 
+                selected={this.state.startDate}
+                onChange={this.handleChange}
+            />
+          </div>
+        );
+    }
+}
+
+export default Birthday;
